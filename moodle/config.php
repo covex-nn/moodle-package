70083c618d4961979dcc240dd50ca9d7fc35c163
Andrey F. Mindubaev <covex.mobile@gmail.com>
2013-04-19 18:37:25 +0400

    config.php, that includes real config.php

---
+++ moodle/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$configLoaded = false;
+/**
+ * developing a package and symlink from vendor-dir to www
+ *
+ * @var array
+ */
+$configPaths = array(
+  dirname(__DIR__) . "/www/config.php", 
+  dirname(__DIR__) . "/../../../www/config.php", 
+);
+foreach ($configPaths as $configPath) {
+  if (file_exists($configPath)) {
+    include($configPath);
+    $configLoaded = true;
+  }
+}
+if (!$configLoaded) {
+  echo "Could not determine a path to real config.php";
+  die();
+}
