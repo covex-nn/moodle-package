366df01b8e30a0df912b180ee1a4a3aa3ac07c4e
Andrey F. Mindubaev <covex.mobile@gmail.com>
2013-05-11 18:03:01 +0400

    Demo module removed

--- src/local/package_demo/db/events.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$handlers = array (
-  'user_created' => array (
-    'handlerfile' => '/local/package_demo/lib.php',
-    'handlerfunction' => 'package_demo_usercreated_handler',
-    'schedule' => 'instant'
-  ),
-);
