f7159eaf56410bb13b06ee2fe65a2932b36add9b
Andrey F. Mindubaev <covex.mobile@gmail.com>
2014-05-15 00:58:37 +0400

    Install moodle with first vagrant up

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,3 +6,7 @@ if [ ! -f build.local.properties ]; then
     cp build.vagrant.properties build.local.properties
 fi
 ant
+
+if [ ! -f web/config.php ]; then
+    ant -f build-dev.xml -Dmysql.root.user=root -Dmysql.root.password=root -Dmoodle.create-database=1 moodle-automation-install
+fi
